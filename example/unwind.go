b5cae811c3c9a23130cc45ee179ed0048372c76a
miliao <miliao@localhost.localdomain>
2019-07-16 03:36:59 -0400

    add unwind test example

---
+++ example/unwind.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"gohook"
+)
+
+//go:noinline
+func bad_func(k int) int {
+	if k <= 0 {
+		panic("bad func vomit\n")
+	}
+
+	return 23 + k
+}
+
+//go:noinline
+func foo(k int) int {
+	return k + bad_func(k)
+}
+
+//go:noinline
+func goo(k int) int {
+	k = -1000 + k
+	return foo_trampoline(k)
+}
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("recover from panic, err:%s\n", err)
+		}
+	}()
+
+	gohook.Hook(foo, goo, foo_trampoline)
+	fmt.Printf("hook info:%s\n", gohook.ShowDebugInfo())
+	foo(-22)
+}
+
+func foo_trampoline(k int) int {
+	defer func(k int) { fmt.Printf("k:%d\n", k)}(k)
+
+	k = -1000 + k
+	return foo_trampoline(k)
+}
